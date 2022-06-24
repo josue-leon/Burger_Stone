f47430f07462baf7c4d910d4a855cce63f6e292d
Mishusoft13 <erika.mastudillo@gmail.com>
2022-06-23 21:12:16 -0500

    Probando :v

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -3,7 +3,6 @@ class Environment {
   static const String BURGER_STONE = "192.168.9.1:3000";
 }
 
-
 //Conexión PC Andrea
 /*class Environment {
   static const String BURGER_STONE = "192.168.100.15:3000";
