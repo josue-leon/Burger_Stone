fac9434830d80abd94d145a109a7eb8e07c345b4
unknown <eli.vizuete@gmail.com>
2022-06-29 14:18:31 -0500

    Fix Tokens

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -4,16 +4,17 @@ class Environment {
   static const String BURGER_STONE = "192.168.9.1:3000";
 }
 */
-/*
+
 //Conexión PC Andrea
 
 class Environment {
   static const String BURGER_STONE = "192.168.100.15:3000";
 }
-*/
+
 
 //Conexión PC Josue
-
+/*
 class Environment {
   static const String BURGER_STONE = "192.168.0.120:3000";
 }
+*/
